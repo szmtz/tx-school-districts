17e854b8c538d7d70132a9811cca6be80b3e90d8
saúl z mtz <97975590+szmtz@users.noreply.github.com>
2023-10-25 21:14:43 +0000

    Add/edit JS to sort the date and not display JSON

--- script.js
+++ script.js
@@ -2,8 +2,23 @@
 var data = [
     { district: "Houston Independent School District (HISD)", students: "209,772" },
     { district: "Dallas Independent School District (DISD)", students: "152,991" },
+    { district: "Cypress-Fairbanks Independent School District (CFISD)", students: "116,000" },
+    { district: "Northside Independent School District (NISD)", students: "105,000" },
+    { district: "Fort Worth Independent School District (FWISD)", students: "78,000" },
+    { district: "Austin Independent School District (AISD)", students: "75,000" },
+    { district: "San Antonio Independent School District (SAISD)", students: "47,000" },
+    { district: "Katy Independent School District (KISD)", students: "84,000" },
+    { district: "Plano Independent School District (PISD)", students: "53,000" },
+    { district: "North East Independent School District (NEISD)", students: "65,000" },
+
     // Add more data as needed
 ];
+
+// Sort the data by the number of students in descending order
+data.sort(function(a, b) {
+    return parseInt(b.students.replace(',', '')) - parseInt(a.students.replace(',', ''));
+});
+
 
 // Get a reference to the table
 var table = document.getElementById("districtTable");
@@ -18,5 +33,5 @@ for (var i = 0; i < data.length; i++) {
 }
 
 // Attach the data to a container with the id "dataContainer"
-var dataContainer = document.getElementById("dataContainer");
-dataContainer.innerHTML = JSON.stringify(data, null, 2); // Display the data as a JSON string
+// var dataContainer = document.getElementById("dataContainer");
+// dataContainer.innerHTML = JSON.stringify(data, null, 2); // Display the data as a JSON string
